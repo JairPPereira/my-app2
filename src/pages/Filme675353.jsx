d2dcd74ff426fab173b2c692c1bd5843d31faf59
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-29 19:17:55 -0300

    gg

--- src/pages/Filme675353.jsx
+++ src/pages/Filme675353.jsx
@@ -1,21 +1,21 @@
 import * as React from "react";
 import ReactAudioPlayer from 'react-audio-player';
 import { Link } from 'react-router-dom';
-import './app.css';
 
-function Passado() {
+
+function Filme675353() {
     return (
       <>
         <main>
           <div className="App">
           <p><br /></p>
         <div className='pele'>
-          <h2>Passado</h2>
+          <h2>Top hits</h2>
           <img class="fit-picture"
             src="https://1.bp.blogspot.com/-mvFZTJD3EqA/YUaZpxM510I/AAAAAAAABHg/EW790K3rrfIxt3KPjpk5kx8WzgqJcstRACLcBGAsYHQ/s0/mradio.png"
             alt="Grapefruit slice atop a pile of other slices"></img>
           <ReactAudioPlayer
-            src="http://stream.zeno.fm/n290bnzvsy8uv.mp3"
+            src="http://stream.zeno.fm/wpgty99taphvv"
             controls
           /></div>
   
@@ -36,4 +36,4 @@ function Passado() {
     );
   }
 
-  export default  Passado;
+  export default  Filme675353;
