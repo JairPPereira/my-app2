fef0693d680df8438331adcc3fc9900b42fe8f53
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-29 21:19:56 -0300

    d

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import App from "./App";
-import Home from "./pages/Home";
-import Movie from "./pages/Movie";
-
-import "./index.css";
-import Filme759175 from "./pages/Filme759175";
-import Filme675353 from "./pages/Filme675353";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="movie/:id" element={<Movie />} />
-          <Route path="filme759175" element={<Filme759175 />} />
-          <Route path="filme675353" element={<Filme675353 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
